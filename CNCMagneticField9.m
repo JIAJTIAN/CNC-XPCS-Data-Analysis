e1ce1854c6ac08c71bc8d4164a39c765e4541d42
Andrew Bae <andrewbae711@gmail.com>
2021-06-09 14:29:44 -0400

    Added graph C vs Tau code

--- CNCMagneticField9.m
+++ CNCMagneticField9.m
@@ -12,7 +12,8 @@ AttArray = {'0.007','0.036','0.190','1.000'};
 EpsArray = {'1.334','9.994','99.994'};
 DoseArrayTota = [0.0093 0.0480 0.0700 0.2535 0.3598 0.7000 1.3340 1.8989 3.5998 9.9940 18.9989 99.9940];
 DoseNum = 6;
-Doselim = 0.1;% this is what is was previouslyDoseArrayTota(DoseNum);
+Doselim = 1;% lower dose limit bound this is what is was previouslyDoseArrayTota(DoseNum);
+Doselim2 = 2; % these two Dose limits ensure doses are in between 1 and 2
 Threshold = 0.2;
 RefinedLength = 6;
 EqDiff = 0.05;
@@ -40,9 +41,9 @@ SaveFolderDose = [ResultFolder,SaveFolderName, SaveFolderNameDose]; % save path 
 
 CelluloseType = 'CNC';
 Solvent = 'PG';
-PlotCell{1} = [9, 12, 17, 22, 32]; % concentration variation
-PlotCell{2} = [23, 24, 25]; % MagneticfieldExposureTime Variation
-PlotCell{3} = [35, 3, 6]; % CaCl2 variation
+PlotCell{1} = [9, 12, 17, 22, 32]; % folders numbers for concentration variation
+PlotCell{2} = [23, 24, 25]; % folder numbers for MagneticfieldExposureTime Variation
+PlotCell{3} = [35, 3, 6]; % folder numbers CaCl2 variation
 
 % CaseNum = 1;
 % Index = find(contains(C,'bla'));;
@@ -53,7 +54,7 @@ if ~exist(SaveFolder, 'dir')
     mkdir(SaveFolder)
 end
 
-% Creat folder to store results
+% Creat folder to store results for Dose study folder
 if ~exist(SaveFolderDose, 'dir')
     mkdir(SaveFolderDose)
 end
@@ -77,7 +78,7 @@ end
     for ExpNumIndex = 1:length(SampleFolderList)
         SubfolderName1 = SampleFolderList{ExpNumIndex};
         
-        RunIndex = 0;
+        RunIndex = 0; % I think this is how many doses are going to be analyzed
         % Sub2. Loop for Att
         for AttIndex = 1:length(AttArray)
             Att = AttArray{AttIndex};
@@ -88,10 +89,10 @@ end
                 Esp = EpsArray{EspIndex};
                 
                 
-                % Dose filter
+                % Dose filter, filtering out doses that do not have good data
                 clear Dose
                 Dose = str2num(Att).*str2num(Esp);
-                if Dose > Doselim
+                if Dose > Doselim && Dose < Doselim2
                    
                 
                     RunIndex = RunIndex + 1;
@@ -142,18 +143,18 @@ end
     
     %% Data Processing and Graphing
     
-    figure;
+   
     for CaseNum = 1:3 % determiens what variable (concentration, time, or CaCl2) is changed
         PlotArray = PlotCell{CaseNum}; % the specific folders for this case
-
+        
         switch CaseNum
             case 1
-                ExpName = 'Concentratioin Variation';
-                Xarray = [1 2 4 6 8];
-                XUnit = 'Concentration(wt%)';
+                ExpName = 'Concentration Variation';
+                Xarray = [1 2 4 6 8]; 
+                XUnit = ' (wt%)';
             case 2
                 ExpName = 'MagneticfieldExposureTime Variation';
-                Xarray = [0 3 28]; % don't see a 1 hour one, there was 1 hour in Xarray in the original
+                Xarray = [0 3 28]; % 
                 XUnit = 'Time(h)';
             case 3
                 ExpName = 'CaCl2 Variation';
@@ -161,8 +162,10 @@ end
                 XUnit = 'CaCl_2 Amount';   
         end % end of the switch statement
         
-        for qNum = 1:8 % go through all of the q values
+        for qNum = 1:8 % go through all of the q values 
             for PlotIndex = 1:length(Xarray) % this determines what folders to go into for this case 
+                ColorVec = 0.8*jet(length(Xarray)); % to be used later in plotting C vs tau
+                
                 ExpNumIndex = PlotArray(PlotIndex); 
                 SavePath = [MainFolder,'\',SampleFolderList{ExpNumIndex}];
                 
@@ -171,8 +174,8 @@ end
                 [SegmentSize,ExpSize] = size(SaveNameCell);  
                 DoseArray = DoseMatrix(:,PlotIndex);
                 [DoseArray,DoseIndex] = sort(DoseArray); % Sort index by dose
-                for SacnIndex = 1:RunIndex % RunIndex = 6
-                    ScanNameCell{SacnIndex} = SaveNameCell{DoseIndex(SacnIndex),ExpNumIndex}; % creates new name cell with only certain cells
+                for SacnIndex = 1:RunIndex 
+                    ScanNameCell{SacnIndex} = SaveNameCell{DoseIndex(SacnIndex),ExpNumIndex}; % creates new name cell with only certain cell
                 end
                 % Read mat file in folders
                 for SegmentIndex = 1:SegmentSize % segment size is the number of different conditions
@@ -220,75 +223,82 @@ end
                         continue
                     end
                 end
-            %% Plot G2 vs. TAU
-                figure;
-                ColorVec = 0.8*jet(9);
-
-                for PlotIndex = 1:RpIndex
-                    G2LineArray(PlotIndex) = line(TauCellRp{PlotIndex},G2CellRp{PlotIndex},...
-                        'LineStyle','none','Marker','o','LineWidth',2,'Color',ColorVec(PlotIndex,:));
-                    G2LengendArray{PlotIndex} = ['Dose = ', num2str(DoseArray(PlotIndex))];
-
+%             %% Plot G2 vs. TAU (Dose Study)
+%                 figure;
+%                 ColorVec = 0.8*jet(3);
+%                 
+%                 for PlotIndex = 1:RpIndex
+%                     G2LineArray(PlotIndex) = line(TauCellRp{PlotIndex},G2CellRp{PlotIndex},...
+%                         'LineStyle','none','Marker','o','LineWidth',2,'Color',ColorVec(PlotIndex,:));
+%                     G2LengendArray{PlotIndex} = ['Dose = ', num2str(DoseArray(PlotIndex))];
+% 
+%                 end
+%                 G2Legend = legend(G2LineArray,G2LengendArray,'Interpreter', 'none');
+%                 set(gca,'XScale','log','XTick',10.^(linspace(-3,2,6)))
+%                 xlabel('\tau [s]')
+%                 xlim([10^-3 10^1])
+%                 ylabel('g_{2}')
+%                 ylim([0.9 1.3])
+%                 title('g2 vs \tau');
+%                 box off
+% 
+%                 FigureTitle = ['Dose_Study_',SampleFolderList{ExpNumIndex},'_qNum=', char(string(qNum))];
+%                 SaveTitle = [FigureTitle,'.png'];
+%                 SavePath = strcat(SaveFolderDose,SaveTitle);
+%                 saveas(gcf,SavePath);
+                
+                %% Data Assembly
+                % Combines the uids
+                
+                AssembleData = SgPackaging(TauCellRp,G2CellRp);
+                TAU = AssembleData(:,1);
+                G2 = AssembleData(:,2);
+                if CaseNum == 1 && PlotIndex == 4 % this gets rid of the 1.8989 dose for the 6% weight concentration because the data was not connected
+                    TAU = TauCellRp{1, 1};
+                    G2 = G2CellRp{1, 1};
                 end
-                G2Legend = legend(G2LineArray,G2LengendArray,'Interpreter', 'none');
-                set(gca,'XScale','log','XTick',10.^(linspace(-3,2,6)))
-                xlabel('\tau [s]')
-                xlim([10^-3 10^1])
-                ylabel('g_{2}')
-                ylim([0.9 2])
-                title('g2 vs \tau');
-                box off
+                
+                
+                % Get rid of zero tau
+                ZeroIndex = TAU == 0; %creates a logical vector
+                TAU(ZeroIndex) = [];
+                G2(ZeroIndex) = [];
+                
+                % Define G2_Inf 
+                BLDataY = G2(end-9:end);
+                G2_Inf = mean(BLDataY); %takes the mean of the last 10 data points and defines that as G2 inf
 
-                FigureTitle = ['Dose_Study_',SampleFolderList{ExpNumIndex},'_qNum=', char(string(qNum))];
-                SaveTitle = [FigureTitle,'.png'];
-                SavePath = strcat(SaveFolderDose,SaveTitle);
-                saveas(gcf,SavePath);
-          end      
-%                 %% Data Assembly
-%                 % Combines the uids
-%                 AssembleData = SgPackaging(TauCellRp,G2CellRp);
-%                 TAU = AssembleData(:,1);
-%                 G2 = AssembleData(:,2);
-%                 
-%                 
-%                 % Get rid of zero tau
-%                 ZeroIndex = TAU == 0; %creates a logical vector
-%                 TAU(ZeroIndex) = [];
-%                 G2(ZeroIndex) = [];
-%                 
-%                 % Define G2_Inf 
-%                 BLDataY = G2(end-9:end);
-%                 G2_Inf = mean(BLDataY); %takes the mean of the last 10 data points and defines that as G2 inf
-% 
-%                 % Beat Correction
-%                 C = (G2-G2_Inf)/(Beta - G2_Inf + 1); %square of G1
-%                 
-%                
-%                 [TAU,SortIndex] = sort(TAU); %sorts Tau from least to greatest
-%                 C = C(SortIndex); %sorts g1 according to tau
-%                
-%                 %% Data Fitting
-%                 FitResult = StrExpFit(TAU, C);
-%         
-%                 CFitKWW = FitResult{1};
-%                 CoefArrayKWW = FitResult{2};
-%                 GammaKWW(qNum) = CoefArrayKWW(1);
-%                 littleGamma(qNum) = CoefArrayKWW(2); % records little Gamma
-%         
-% 
-%                 CFitSingle = FitResult{4};
-%                 CoefArraySingle = FitResult{5};
-%                 GammaSingle(qNum) = CoefArraySingle(1);
-% 
-%                 qArray(qNum) = qValue;
-%                 PlotTAU{qNum} = TAU;
-%                 PlotG2{qNum} = C;
-%                 PlotG2FitKWW{qNum} = CFitKWW;
-%                 PlotG2FitSingle{qNum} = CFitSingle;
+                % Beat Correction
+                C = (G2-G2_Inf)/(Beta - G2_Inf + 1); %square of G1
+                
+               
+                [TAU,SortIndex] = sort(TAU); %sorts Tau from least to greatest
+                C = C(SortIndex); %sorts g1 according to tau
+               
+                %% Data Fitting
+                FitResult = Str_and_Sin_exp(TAU, C); % fits the data with streched and single exponential
+                % Str_and_Sin_exp was renamed from something like StrExp because both streched and single exponential fittings are done
+                
+                CFitKWW = FitResult{1};
+                CoefArrayKWW = FitResult{2};
+                GammaKWW(PlotIndex) = CoefArrayKWW(1);
+                littleGamma(PlotIndex) = CoefArrayKWW(2); % records little Gamma
+        
+
+                CFitSingle = FitResult{4};
+                CoefArraySingle = FitResult{5};
+                GammaSingle(PlotIndex) = CoefArraySingle(1);
+
+                % Data to be used in the plots
+                qArray(PlotIndex) = qValue;
+                PlotTAU{PlotIndex} = TAU;
+                PlotG2{PlotIndex} = C;
+                PlotG2FitKWW{PlotIndex} = CFitKWW;
+                PlotG2FitSingle{PlotIndex} = CFitSingle;
+             end   
                 
                 
-                
-                    % I don't think the rest of this is old stuff
+                    % I think the rest of this is old stuff
 %                 %% G2 Data Baseline Refine Process
 %                 [SegmentSizeBase,UidSizeBase] = size(g2Value_RefinedRaw);
 %                 for SegmentIndex = 1:SegmentSizeBase
@@ -399,8 +409,26 @@ end
 % 
 %             end
 
-        end % ends the q array
         
+      %% Plot Fit C vs Tau  
+      figure; 
+        for PlotIdx = 1:length(Xarray) % data is fitted witht he streched exponential
+            KWWLineArray(PlotIdx) = line(PlotTAU{PlotIdx}, PlotG2{PlotIdx}, 'LineStyle','none','Marker','o','LineWidth',2,'Color',ColorVec(PlotIdx,:));
+            KWWFitLineArray(PlotIdx) = line(PlotTAU{PlotIdx}, PlotG2FitKWW{PlotIdx}, 'LineWidth',2,'Color',ColorVec(PlotIdx,:));
+            KWWLegendArray{PlotIdx} = append(num2str(Xarray(PlotIdx)), XUnit); 
+        end
+        
+        xlabel( '\tau [s]');
+        ylabel('C');
+        G2Legend = legend(KWWLineArray, KWWLegendArray,'Interpreter', 'none');%,'Location','northwest');
+        set(gca,'XScale','log') % sets x scale to logarithimic
+        
+        FigureTitle = ['C vs tau ', ExpName, ' qNum = ', num2str(qNum)];
+        title(FigureTitle);
+        SaveTitle = ['C vs tau_', ExpName, '_qNum_', num2str(qNum), '.png'];
+        SavePath = strcat(SaveFolder,SaveTitle);
+        saveas(gcf,SavePath);
+        clear KWWLegendArray % clear the legend array so legend stuff from previous case doesn't carry over to next case
         
 %     G2Legend = legend(G2LineArray,G2LengendArray,'Interpreter', 'none');
 %     set(gca,'XScale','log','XTick',10.^(linspace(-3,2,6)))
@@ -419,7 +447,7 @@ end
 %     
 %     % Store D Array
 %     DCell{CaseNum} = DArray;
-
+    end % ends the q array
 end % end of the for loop for the different cases
 
 
